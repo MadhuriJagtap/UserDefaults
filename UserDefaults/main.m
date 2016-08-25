267334ee5622d81ea09ca1bc8f607ca490f44947
MadhuriJagtap <madhuri@felix-its.com>
2016-08-25 09:35:48 +0530

    Initial Commit

---
+++ UserDefaults/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UserDefaults
+//
+//  Created by Felix-Madhuri on 25/08/16.
+//  Copyright © 2016 Felix-Madhuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
